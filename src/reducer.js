e2a4b46ee14d19d7834dba51d4c04315554213a4
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-02-26 12:50:39 +0530

    Initial commit

---
+++ src/reducer.js
@@ -0,0 +1,43 @@
+//[] simple array to represent store
+// with if else
+let lastId=0;
+
+export default function reducer(state = [], action){
+    if(action.type === 'bugAdded')
+    return[
+        ...state,
+        {
+            id : ++lastId,
+            description : action.payload.description,
+            resolved : false
+        }
+    ];
+    else if(action.type === 'bugRemoved')
+    return state.filter(bug=> bug.id !== action.payload.id);
+    return state;
+}
+
+//with switch
+
+// let lastId=0;
+
+// function reducer(state = [], action){
+//     switch (action.type){
+//         case "bugAdded":
+//         return[
+//         ...state,
+//         {
+//             id : ++lastId,
+//             description : action.payload.description,
+//             resolved : false
+//         }
+//     ];
+//     case "bugRemoved":
+//         return state.filter(bug=> bug.id !== action.payload.id);
+//     default : return state;
+//     }
+   
+// }
+
+
+
