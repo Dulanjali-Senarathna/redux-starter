1fd1f46b22489c6b194918e4dead58e27df7cc6f
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2023-03-01 11:14:15 +0530

    Exercise done - add toast middleware

--- src/index.js
+++ src/index.js
@@ -26,9 +26,14 @@ import { applyMiddleware } from 'redux';
 
 const store = configureStore();
 
-store.dispatch((dispatch, getState) =>{
-    dispatch({type: 'bugsReceived', bugs: [1,2,3]});
-    console.log(getState());
+//  store.dispatch((dispatch, getState) =>{
+//      dispatch({type: 'bugsReceived', bugs: [1,2,3]});
+//      console.log(getState());
+//  });
+
+store.dispatch({
+    type:"error",
+    payload: {message: "An error occured"}
 });
 
 // store.state = 1;
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -2,6 +2,7 @@ import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import reducer from "./reducer";
 import logger from "./middleware/logger";
 import func from "./middleware/func";
+import toast from "./middleware/toast";
 
 
 
@@ -12,6 +13,7 @@ return configureStore({
     middleware : [
         ...getDefaultMiddleware(),
         logger({destination : "console"}),
+        toast
         
     ]
 });
